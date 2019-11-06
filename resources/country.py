5c3d2b4791eaa1acac35b987ba8bf1a1535e7925
Robert Morgan <robertbrmorgan@hotmail.co.uk>
2019-11-06 14:56:33 +0800

    fixed country delete and removed titles from country table

--- resources/country.py
+++ resources/country.py
@@ -11,11 +11,6 @@ class Country(Resource):
                         required=True,
                         help="This field is required"
                         )
-    parser.add_argument('country_name',
-                        type=str,
-                        required=True,
-                        help="This field is required"
-                        )
 
     # get the country code when supplying a country name
     def get(self, country_name):
@@ -26,14 +21,37 @@ class Country(Resource):
 
     # post a country code when supplying a country name
     def post(self, country_name):
-        pass
+        if CountryModel.find_by_name(country_name):
+            return {'message': "A country with name '{}' already exists.".format(country_name)}, 400
 
-    # put a country code when supplying a country name
-    def put(self, country_name):
-        pass
+        data = Country.parser.parse_args()
+
+        country = CountryModel(data['country_code'], country_name)
+
+        try:
+            country.save_to_db()
+        except:
+            return {'message': "An error occurred inserting the country."}, 500
+        return country.json(), 201
 
     def delete(self, country_name):
-        pass
+        country = CountryModel.find_by_name(country_name)
+        if country:
+            country.delete_from_db()
+            return {'message': "Country '{}' deleted".format(country_name)}, 200
+        return {'message': "Country '{}' not found.".format(country_name)}, 404
+
+    def put(self, country_name):
+        data = Country.parser.parse_args()
+        country = CountryModel.find_by_name(country_name)
+
+        if country:
+            country.country_code = data['country_code']
+        else:
+            country = CountryModel(data['country_code'], country_name)
+
+        country.save_to_db()
+        return country.json()
 
 
 class CountryList(Resource):
