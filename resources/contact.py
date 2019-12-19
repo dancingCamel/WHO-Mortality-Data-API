042bc5d97d8b3e16753d2123e5c44430acdc7f7f
Robert Morgan <robertbrmorgan@hotmail.co.uk>
2019-12-19 15:15:21 +0800

    set up contact form

--- resources/contact.py
+++ resources/contact.py
@@ -6,3 +6,9 @@ class Contact(Resource):
     def get(self):
         headers = {'Content-Type': 'text/html'}
         return make_response(render_template('contact.html'), 200, headers)
+
+    def post(self):
+        # handle email sending here
+        # get info from form
+        # redirct to contact page with either an 'error' or a 'info' flash
+        pass
