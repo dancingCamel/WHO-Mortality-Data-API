121cdf78e548cc95326878ae3d96ccc25a09640a
Robert Morgan <robertbrmorgan@hotmail.co.uk>
2020-01-06 23:07:20 +0800

    add save and delete methods to blacklist class

--- models/blacklist.py
+++ models/blacklist.py
@@ -16,3 +16,15 @@ class BlacklistModel(db.Model):
     @classmethod
     def find_by_api_key(cls, api_key):
         return cls.query.filter_by(api_key=api_key).first()
+
+    @classmethod
+    def find_all(cls):
+        return cls.query.all()
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete_from_db(self):
+        db.session.delete(self)
+        db.session.commit()
